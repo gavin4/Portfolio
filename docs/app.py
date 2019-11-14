196359f4020a489f4c03d99b446e137347e99d8a
gavin4 <38709505+gavin4@users.noreply.github.com>
2019-11-14 10:59:17 -0800

    Update app.py

--- docs/app.py
+++ docs/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'hello world'
+def index():
+  return render template('index.html')
 
 if __name__ =='__main__':
   app.run()
